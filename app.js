466902edcc7a8db78f2532a3d177a42607ec4652
karthikq <kk968347@gmail.com>
2021-03-29 19:36:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+/** @format */
+const express = require("express");
+const bodyParser = require("body-parser");
+const Post = require("./models/postsmodel");
+
+var cookieParser = require("cookie-parser");
+require("./database/conect");
+var session = require("express-session");
+const app = express();
+require("dotenv").config();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 3000 },
+  })
+);
+
+app.use("/", require("./router/HomeRoute"));
+app.use("/user", require("./router/UsersRoute"));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log("server is running");
+});
